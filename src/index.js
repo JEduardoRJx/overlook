e2bcfbd81568e26bdd092ea30f94b8ec6a085f16
Eduardo <jeduardorjx@gmail.com>
2019-09-14 11:45:24 -0600

    Setup basic structure for fetch and promise

--- src/index.js
+++ src/index.js
@@ -8,12 +8,31 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+import domUpdates from './domUpdates';
+
 
 console.log('This is the JavaScript entry file - your code begins here.');
 //CODE BELOW
+let usersResponse = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then(response => response.json());
+let roomsResponse = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms').then(response => response.json());;
+let bookingResponse = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then(response => response.json());;
+let roomServiceResponse = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices').then(response => response.json());
+let hotelData = {users: [], rooms: [], bookings: [], roomServices: []};
 
-// let 
+Promise.all([usersResponse, roomsResponse, bookingResponse, roomServiceResponse])
+  .then(data => {
+    hotelData['users'] = data[0].users;
+    hotelData['rooms'] = data[1].rooms;
+    hotelData['bookings'] = data[2].bookings;
+    hotelData['roomServices'] = data[3].roomServices
+    return hotelData
+  })
+  .then(hotelData => {
+    customer
+  })
+  .then(hotelData => console.log("hotelData", hotelData))
+  .catch(error => console.log(error));
 
 //Hide all the sections besides the MAIN section on page load
 $('#main').show().siblings('section').hide();
@@ -31,6 +50,11 @@ let getDate = () => {
   let date = new Date();
   return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 }
+
+// let bookings = new Bookings()
+// function displayTotalRoomsAvailable(date) {
+domUpdates.displayTotalRoomsAvailable(getDate());
+// }
 
 $('.todays-date').text(getDate)
 
