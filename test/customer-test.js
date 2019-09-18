083f018902523bcf42f32095726609c5a6df7338
Eduardo <jeduardorjx@gmail.com>
2019-09-18 05:06:58 -0600

    Testing to make sure Customers is a function

--- test/customer-test.js
+++ test/customer-test.js
@@ -1,5 +1,17 @@
 import chai from 'chai';
-import Customer from '../src/customer.js';
+import Customers from '../src/customers.js';
 import sampleData from '../src/sampleData.js';
 const expect = chai.expect;
 
+describe('Customers', () => {
+  let customers;
+  beforeEach(() => {
+    customers = new Customers(sampleData);
+  });
+
+  it('should be a function', () => {
+    expect(Customers).to.be.a('function');
+  });
+
+
+});
