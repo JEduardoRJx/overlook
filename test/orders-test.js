19e267a2b982aedea86f79977ed6a6c41d578464
Eduardo <jeduardorjx@gmail.com>
2019-09-18 02:49:17 -0600

    Test it should be a function

---
+++ test/orders-test.js
@@ -0,0 +1,57 @@
+import chai from 'chai';
+import Orders from '../src/orders.js';
+import sampleData from '../src/sampleData.js';
+const expect = chai.expect;
+
+describe('Orders', () => {
+  let orders;
+  beforeEach(() => {
+    orders = new Orders(sampleData);
+  });
+
+  it('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+  it.skip('should be a function', () => {
+    expect(Orders).to.be.a('function');
+  });
+
+
+});
